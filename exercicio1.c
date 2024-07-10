81983eace390333ee9201e6c0d95b8a2f9416cf5
eduardodaguinha <eduardosousadaga@icloud.com>
2024-07-09 22:03:41 -0300

    Upload do Exercício 1

---
+++ exercicio1.c
@@ -0,0 +1,28 @@
+// Enunciado: Faça um programa que receba quatro números inteiros, calcule e mostre a soma desses números.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main(void) {
+    setlocale(LC_ALL, "pt_BR.UTF-8");
+    int n1, n2, n3, n4, soma;
+    
+    // Recebendo os valores
+    printf("Oi, meu nome é Dudu e irei calcular a soma de 4 números inteiros!\n");
+    printf("Digite o primeiro número:\n");
+    scanf("%d", &n1);
+    printf("Agora, digite o segundo número:\n");
+    scanf("%d", &n2);
+    printf("Digite o terceiro número:\n");
+    scanf("%d", &n3);
+    printf("Por fim, digite o quarto número:\n");
+    scanf("%d", &n4);
+
+    // Realizando a soma
+    printf("Por favor, aguarde...\n");
+    soma = n1 + n2 + n3 + n4;
+    printf("A soma dos números digitados é %d.", soma);
+
+    return 0;
+}
